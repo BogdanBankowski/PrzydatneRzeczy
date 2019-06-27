e844a8a28d3df94f58c6fb95a2a7ef323ec22b37
Bogdan Bańkowski <Hevixxx@gmail.com>
2019-06-27 11:13:22 +0200

    Likes are working
    Still not display on the homepage and raise_likes function
    needs some solid refactor(cut it into few smaller functions)

--- data_handler.py
+++ data_handler.py
@@ -1,15 +1,15 @@
 import os
-import csv
+import fileinput
 
 DATA_HEADERS = ['ID','Nazwa', 'Link', 'Opis', 'Tagi', 'Polubienia']
 DATA_TO_LOAD_FROM_USER = ['Nazwa', 'Link', 'Opis', 'Tagi']
 DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data.csv'
-
+LIKES_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'likes.csv'
 def get_whole_table():
     result = []
     with open(DATA_FILE_PATH, 'r') as file:
         for line in file.readlines():
-            result.append(line.strip().split(';'))
+            result.append(line.split(';'))
     return result
 
 def add_data_to_file(data):
@@ -21,3 +21,21 @@ def add_data_to_file(data):
             if i != len(data)-1:
                 file.write(';')
             i += 1            #Taka logika zeby nie dodawal ';' na koncu linijki bo dodawalo to pusty wyraz do tabeli
+
+def raise_likes_quantity_by_one(post_id):
+    with open(LIKES_FILE_PATH, 'r') as file:
+        post_id = post_id[1:-1]
+        likes_dict = {}
+        for line in file.readlines():
+            line_as_list = line.split(';')
+            likes_dict[line_as_list[0]] = int(line_as_list[1])
+        likes_dict[post_id] += 1
+    with open(LIKES_FILE_PATH, 'w') as file:
+        for key in likes_dict.keys():
+            file.write(key+';'+str(likes_dict[key])+'\n')
+
+
+
+
+
+
--- server.py
+++ server.py
@@ -4,10 +4,12 @@ import uuid
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
     data = data_handler.get_whole_table()
-    return render_template('homepage.html', data=data, DATA_HEADERS = data_handler.DATA_HEADERS)
+    return render_template('homepage.html', data=data, DATA_HEADERS=data_handler.DATA_HEADERS)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -17,16 +19,20 @@ def add():
         data_to_add.append(unique_id)
         for i in range(len(data_handler.DATA_TO_LOAD_FROM_USER)):
             data_to_add.append(request.form[data_handler.DATA_TO_LOAD_FROM_USER[i]])
-        data_to_add.append('0')
+        data_to_add.append(str(likes[data_to_add[0]]))
         data_handler.add_data_to_file(data_to_add)
         return redirect('/')
-    return render_template('add.html')
+    return render_template('add.html', likes=likes)
 
 
+@app.route('/like/<post_id>')
+def like(post_id):
+    data_handler.raise_likes_quantity_by_one(post_id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
     app.run(
-        debug=True, # Allow verbose error reports
-        port=5000 # Set custom port
+        debug=True,  # Allow verbose error reports
+        port=5000  # Set custom port
     )
